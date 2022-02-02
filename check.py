fe58d83c512b39e4ee4b07ea63bd2afc7cd41692
vr4eslav <kldgrcorp@gmail.com>
2022-02-02 23:45:18 +0300

    I removed the constant from the function and renamed it, also added type hints, and error logging at the exception level. Complex refactoring according to PEP

--- check.py
+++ check.py
@@ -2,8 +2,13 @@ import re
 from time import sleep
 from datetime import datetime
 from urllib.request import urlopen
+from loguru import logger
 
 # No proxy
+
+SATOSHI_PER_BTC: float = 1e8
+
+
 def check_balance_bc(address):
     try:
         blockchain_tags_json = [
@@ -11,10 +16,8 @@ def check_balance_bc(address):
             "final_balance",
         ]
 
-        SATOSHIS_PER_BTC = 1e8
-
-        check_address = address
-        reading_state = 1
+        check_address: object = address
+        reading_state: int = 1
         # keeps reading till OK
         while reading_state:
             try:
@@ -24,7 +27,8 @@ def check_balance_bc(address):
                 )
                 htmltext = htmlfile.read().decode("utf-8")
                 reading_state = 0
-            except:
+            except Exception as err:
+                logger.exception(err)
                 reading_state += 1
                 sleep(60 * reading_state)
 
@@ -35,21 +39,24 @@ def check_balance_bc(address):
                 blockchain_info_array.append(
                     float(re.search(r'%s":(\d+),' % tag, htmltext).group(1))
                 )
-        except:
+        except Exception as err:
+            logger.exception(err)
             pass
         for i, btc_tokens in enumerate(blockchain_info_array):
 
             if btc_tokens > 0.0:
-                return btc_tokens / SATOSHIS_PER_BTC
+                return btc_tokens / SATOSHI_PER_BTC
             else:
                 return 0
-    except:
+    except Exception as err:
+        logger.exception(err)
         pass
+
+
 def check_balance_btc(address):
     # get json data from https://chain.api.btc.com/v3/address/1assTGVhuCnrix5LvhL2GXEkSS3fS2XBT and parse it
     # to get the balance
     try:
-        address = address
         reading_state = 1
         while reading_state:
             try:
@@ -59,17 +66,18 @@ def check_balance_btc(address):
                 )
                 htmltext = htmlfile.read().decode("utf-8")
                 reading_state = 0
-            except:
+            except Exception as err:
+                logger.exception(err)
                 reading_state += 1
                 sleep(60 * reading_state)
         balance = float(re.search(r'"balance":(\d+\.\d+),', htmltext).group(1))
         return balance
-    except:
+    except Exception as err:
+        logger.exception(err)
         return None
 
 
 def last_seen_bc(address):
-    
     try:
         address = address
         reading_state = 1
@@ -81,12 +89,14 @@ def last_seen_bc(address):
                 )
                 htmltext = htmlfile.read().decode("utf-8")
                 reading_state = 0
-            except:
+            except Exception as err:
+                logger.exception(err)
                 reading_state += 1
                 sleep(60 * reading_state)
         ts = int(htmltext)
         if ts == 0:
             return 0
         return str(datetime.utcfromtimestamp(ts).strftime("%Y-%m-%d %H:%M:%S"))
-    except:
+    except Exception as err:
+        logger.exception(err)
         return None
